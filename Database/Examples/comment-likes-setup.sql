ce3a9845445ee419e491feab40364c5fda84c70a
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-01-06 12:10:41 +0900

    Add PRIMARY KEY in post-likes-setup.sql and comment-likes-setup.sql file

--- Database/Examples/comment-likes-setup.sql
+++ Database/Examples/comment-likes-setup.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS comment_likes (
   user_id INT NOT NULL,
   comment_id INT NOT NULL,
+  PRIMARY KEY (user_id, comment_id),
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
   FOREIGN KEY (comment_id) REFERENCES comments(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
