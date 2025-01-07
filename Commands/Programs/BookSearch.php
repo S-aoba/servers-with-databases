dbfbc46475c8865d7925b0453bc7fa3cfe372b7c
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-01-07 22:13:05 +0900

    Add fetchBookData, saveBookData and getCachedData functionally

--- Commands/Programs/BookSearch.php
+++ Commands/Programs/BookSearch.php
@@ -4,7 +4,9 @@ namespace Commands\Programs;
 
 use Commands\AbstractCommand;
 use Commands\Argument;
+use Database\MySQLWrapper;
 use Exception;
+use JsonException;
 
 class BookSearch extends AbstractCommand {
 
@@ -23,16 +25,18 @@ class BookSearch extends AbstractCommand {
     $isbn = $this->getArgumentValue('isbn');
 
     if($isbn === false) throw new Exception('Please input of the isbn word.');
-    // キャッシュの確認
-    $isCahedBookData = $this->checkCacheData($isbn);
-    
-    if(count($isCahedBookData) > 0) {
-      // $this->printBookData($isCahedBookData);
-    }
 
+    // キャッシュの確認
+    $cahedBookData = $this->getCacheData($isbn);
+    
+    if(count($cahedBookData) > 0) {
+      // キャッシュデータの保存期間が30日を超えていたら、新しいデータを取得して保存する
+      // $this->printBookData($cahedBookData);
+    }
     else {
       // Open Library APIからBook Dataを取得する
       $bookData = $this->fetchBookData($isbn);
+      $this->saveBookDataToDB($bookData, $isbn);
       // $this->printBookData($bookData);
     }
 
@@ -44,30 +48,45 @@ class BookSearch extends AbstractCommand {
 
     $result = file_get_contents($url);
     $data = json_decode($result, true);
+    if(empty($data)) throw new Exception("{$isbn} is not exists. Please input another isbn.");
 
-    if(isset($data["ISBN:{$isbn}"])){
-      $book = $data["ISBN:{$isbn}"];
-      // Titile, Authors, Publishers ,Publish Date のみを抽出してDBへ保存する
-      $formattedBookData = [
-        'Title' => $book['title'],
-        'Authors' => $book['authors'],
-        'Publishers' => $book['publishers'],
-        'PublishDate' => $book['publish_date']
-      ];
-      
-      return $formattedBookData;
-    }
+    $book = $data["ISBN:{$isbn}"];
+    // Titile, Authors, Publishers ,Publish Date のみを抽出してDBへ保存する
+    $formattedBookData = [
+      'Title' => $book['title'],
+      'Authors' => $book['authors'],
+      'Publishers' => $book['publishers'],
+      'PublishDate' => $book['publish_date']
+    ];
     
-    return [];
+    return $formattedBookData;
   }
 
 
-  private function saveBookData(): bool{
-    return true;
+  private function saveBookDataToDB(array $data, string $isbn): void{
+    $mysql = new MySQLWrapper();
+    $id = "book-search-isbn-{$isbn}";
+    $jsonData = json_encode($data);
+
+    $command = "INSERT INTO book_searchs (id, data) VALUES (?, ?)";
+
+    $stmt = $mysql->prepare($command);
+    $stmt->bind_param("ss", $id, $jsonData);
+    $stmt->execute();
+
+    $stmt->close();
   }
 
-  private function checkCacheData(string $isbn): array {
-    return [];
+  private function getCacheData(string $isbn): array {
+    $mysql = new MySQLWrapper();
+    $id = "book-search-isbn-{$isbn}";
+    $command = "SELECT * FROM book_searchs WHERE id = '{$id}'";
+
+    $result = $mysql->query($command)->fetch_all(MYSQLI_ASSOC);
+    
+    if(empty($result)) return [];
+    
+    return $result;
   }
 
   private function updateCahceData(): bool {
