95c6693d04554e7d6dea492860ec4aa318266f58
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-01-09 21:28:24 +0900

    Add code generate of seeder file

---
+++ Database/Seeds/Seeder.php
@@ -0,0 +1,19 @@
+<?php
+namespace Database\Seeds;
+
+use Database\AbstractSeeder;
+
+class Seeder extends AbstractSeeder {
+
+    // TODO: tableName文字列を割り当ててください。
+    protected ?string $tableName = null;
+
+    // TODO: tableColumns配列を割り当ててください。
+    protected array $tableColumns = [];
+
+    public function createRowData(): array
+    {
+        // TODO: createRowData()メソッドを実装してください。
+        return [];
+    }
+}       
