f81875b24e3b62798ad36754df3ac5c565230ed3
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-01-09 21:06:57 +0900

    Add cars and car_parts table

---
+++ Database/Migrations/2025-01-09_1736424159_CreateCarPartTable1.php
@@ -0,0 +1,31 @@
+<?php
+namespace Database\Migrations;
+
+use Database\SchemaMigration;
+
+class CreateCarPartTable1 implements SchemaMigration
+{
+    public function up(): array
+    {
+        // マイグレーションロジックをここに追加してください
+        return [
+            "CREATE TABLE IF NOT EXISTS car_parts (
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                name VARCHAR(50) NOT NULL,
+                description VARCHAR(50),
+                price FLOAT,
+                quantity_in_stock INT,
+                car_id INT NOT NUll,
+                FOREIGN KEY (car_id) REFERENCES cars(id) ON DELETE CASCADE
+            )"
+        ];
+    }
+
+    public function down(): array
+    {
+        // ロールバックロジックを追加してください
+        return [
+            "DROP TABLE car_parts"
+        ];
+    }
+}
