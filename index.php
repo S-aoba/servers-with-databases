7ae66ba170733e057e0acb0c70b69da8c15fe520
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-01-11 13:25:38 +0900

    Implement HTTP rendering system with HTML and JSON support; refactor routing to utilize new renderers and improve error handling in index.php

--- index.php
+++ index.php
@@ -6,30 +6,48 @@ spl_autoload_register(function($class) {
     if (file_exists(stream_resolve_include_path($file))) include($file);
 });
 
-// ルートをロードします。
+$DEBUG = true;
+
+// ルートを読み込みます。
 $routes = include('Routing/routes.php');
 
 // リクエストURIを解析してパスだけを取得します。
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $path = ltrim($path, '/');
 
-// ルートにパスが存在するかチェックします。
+// ルートにパスが存在するかチェックする
 if (isset($routes[$path])) {
-    $view = $routes[$path];
-    $viewPath = sprintf("%s/Views/%s.php",__DIR__, $view);
+    // コールバックを呼び出してrendererを作成します。
+    $renderer = $routes[$path]();
 
-    if (file_exists($viewPath)) {
+    try{
         // ヘッダーを設定します。
-        include 'Views/layout/header.php';
-        include $viewPath;
-        include 'Views/layout/footer.php';
-    } else {
+        foreach ($renderer->getFields() as $name => $value) {
+            // ヘッダーに対する単純な検証を実行します。
+            // PHP8.1以降は非推奨の書き方
+            // $sanitized_value = filter_var($value, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+            $sanitized_value = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+
+            if ($sanitized_value && $sanitized_value === $value) {
+                header("{$name}: {$sanitized_value}");
+            } else {
+                // ヘッダー設定に失敗した場合、ログに記録するか処理します。
+                // エラー処理によっては、例外をスローするか、デフォルトのまま続行することもできます。
+                http_response_code(500);
+                if($DEBUG) print("Failed setting header - original: '$value', sanitized: '$sanitized_value'");
+                exit;
+            }
+
+            print($renderer->getContent());
+        }
+    }
+    catch (Exception $e){
         http_response_code(500);
-        printf("<br>debug info:<br>%s<br>%s", "Internal error, please contact the admin.");
+        print("Internal error, please contact the admin.<br>");
+        if($DEBUG) print($e->getMessage());
     }
 } else {
-    // 一致するルートがない場合、404エラーを表示する
+    // マッチするルートがない場合、404エラーを表示します。
     http_response_code(404);
     echo "404 Not Found: The requested route was not found on this server.";
-    printf("<br>debug info:<br>%s<br>%s", json_encode($routes),$path);
 }
