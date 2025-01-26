0b4c06bbd07886fa95898eb96487edb755299fa3
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-01-26 22:08:21 +0900

    Add DAO(4)

--- Routing/routes.php
+++ Routing/routes.php
@@ -5,19 +5,27 @@ use Helpers\ValidationHelper;
 use Response\HTTPRenderer;
 use Response\Render\HTMLRenderer;
 use Response\Render\JSONRenderer;
+use Database\DataAccess\Implementations\ComputerPartDAOImpl;
 
 return [
     'random/part'=>function(): HTTPRenderer{
-        $part = DatabaseHelper::getRandomComputerPart();
+        $partDao = new ComputerPartDAOImpl();
+        $part = $partDao->getRandom();
 
-        return new HTMLRenderer('component/random-part', ['part'=>$part]);
+        if($part === null) throw new Exception('No parts are available!');
+
+        return new HTMLRenderer('component/computer-part-card', ['part'=>$part]);
     },
     'parts'=>function(): HTTPRenderer{
         // IDの検証
         $id = ValidationHelper::integer($_GET['id']??null);
 
-        $part = DatabaseHelper::getComputerPartById($id);
-        return new HTMLRenderer('component/parts', ['part'=>$part]);
+        $partDao = new ComputerPartDAOImpl();
+        $part = $partDao->getById($id);
+
+        if($part === null) throw new Exception('Specified part was not found!');
+
+        return new HTMLRenderer('component/computer-part-card', ['part'=>$part]);
     },
     'api/random/part'=>function(): HTTPRenderer{
         $part = DatabaseHelper::getRandomComputerPart();
