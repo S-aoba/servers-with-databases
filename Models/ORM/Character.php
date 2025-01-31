f229e913693f4aa382d27ca4a064071dd8d66453
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-01-31 16:06:56 +0900

    ORM(4)

--- Models/ORM/Character.php
+++ Models/ORM/Character.php
@@ -4,4 +4,17 @@ namespace Models\ORM;
 
 use Database\DataAccess\ORM;
 
-class Character extends ORM{}
+class Character extends ORM
+{
+    public function profile(): string
+    {
+        return sprintf(
+            "Name: %s\nDescription: %s\nGender: %s\nSubclass: %s\nRace: %s",
+            $this->name,
+            $this->description,
+            $this->gender,
+            $this->subclass,
+            $this->race
+        );
+    }
+}
